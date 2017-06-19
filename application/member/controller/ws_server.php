4f6579b342c1ce02912d8084c2d39124a80ab425
min6909 <min6909@163.com>
2017-06-19 12:51:33 +0800

    聊天室测试1

--- application/member/controller/ws_server.php
+++ application/member/controller/ws_server.php
@@ -3,12 +3,21 @@
 $server = new swoole_websocket_server("0.0.0.0", 2347);
 
 $server->on('open', function (swoole_websocket_server $server, $request) {
-    echo "server: handshake success with fd{$request->fd}\n";
+//    echo "server: handshake success with fd{$request->fd}\n";
+    file_put_contents( __DIR__ .'/log.txt' , $request->fd);
 });
 
 $server->on('message', function (swoole_websocket_server $server, $frame) {
-    echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
-    $server->push($frame->fd, $frame->data);
+//    echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
+
+    $data = $frame->data;
+    $m = file_get_contents(__DIR__ . '/log.txt');
+    for ($i = 1; $i <= $m; $i++) {
+        echo PHP_EOL . '  i is  ' . $i . '  data  is ' . $data . '  m = ' . $m;
+        $server->push($i, $data);
+    }
+    
+//    $server->push($frame->fd, $frame->data);
 });
 //
 //$server->on('close', function ($ser, $fd) {
