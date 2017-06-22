8ed5be53645d6f2359eef83f56abd0233c24d7c2
min6909 <min6909@163.com>
2017-06-23 01:16:57 +0800

    聊天室测试3： 添加发言时间 添加屏蔽字 修复滚动条bug

--- application/member/controller/Index.php
+++ application/member/controller/Index.php
@@ -7,6 +7,7 @@ use think\View;
 class Index extends Controller{
     public function index(){
         exec("php /usr/share/nginx/html/itrookie/application/member/controller/ws_server.php");
+       
         return $this->fetch();
     }
     
--- application/member/controller/ws_server.php
+++ application/member/controller/ws_server.php
@@ -1,29 +1,28 @@
 <?php
 //创建websocket服务器对象，监听0.0.0.0:9502端口
-$server = new swoole_websocket_server("0.0.0.0", 2347);
+    $server = new swoole_websocket_server("0.0.0.0", 2347);
 
-$server->on('open', function (swoole_websocket_server $server, $request) {
-//    echo "server: handshake success with fd{$request->fd}\n";
-    file_put_contents( __DIR__ .'/log.txt' , $request->fd);
-});
+    $server->on('open', function (swoole_websocket_server $server, $request) {
+    //    echo "server: handshake success with fd{$request->fd}\n";
+    });
 
-$server->on('message', function (swoole_websocket_server $server, $frame) {
-//    echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
+    $server->on('message', function (swoole_websocket_server $server, $frame) {
+    //    echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
 
     $data = $frame->data;
-    $m = file_get_contents(__DIR__ . '/log.txt');
-    for ($i = 1; $i <= $m; $i++) {
-        echo PHP_EOL . '  i is  ' . $i . '  data  is ' . $data . '  m = ' . $m;
-        $server->push($i, $data);
+    $pattern = '/[艹|操|淫|荡|奸]/u';
+    $data =preg_replace($pattern,'*',$data);
+    foreach ($server->connections as $fd) {
+    //        echo PHP_EOL . '  i is  ' . $k . '  data  is ' . $data;
+    $server->push($fd, $data);
     }
-    
-//    $server->push($frame->fd, $frame->data);
-});
-//
-//$server->on('close', function ($ser, $fd) {
-//    echo "client {$fd} closed\n";
-//});
 
-$server->start();
+    //    $server->push($frame->fd, $frame->data);
+    });
+    //
+    $server->on('close', function ($server, $fd) {
+    //    echo "client {$fd} closed\n";
+    });
 
+    $server->start();
 
